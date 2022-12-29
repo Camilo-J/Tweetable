2d867c228ebaab729070946df6fd1263b00a2cc0
Camilo-J <josecamilo1902@outlook.com>
2022-12-29 18:53:40 -0500

    Move to folder api/v1

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -3,15 +3,11 @@ class LikesController < ApplicationController
     @tweet = Tweet.find(params[:tweet])
     @new_like = Like.new(user: current_user, tweet: @tweet)
     @new_like.save
-
-    redirect_to root_path
   end
 
   def destroy
     @tweet = Tweet.find(params[:id])
     @like_got = Like.find_by(user: current_user, tweet: @tweet)
     @like_got.destroy
-
-    redirect_to root_path
   end
 end
---
+++ app/controllers/api/v1/tweets_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class TweetsController < ApplicationController
+      skip_before_action :authenticate_user!, only: %i[index show]
+      def index
+        @tweets = Tweet.all.order(updated_at: :desc).where(replied_to_id: nil)
+        render json: @tweets
+      end
+
+      def show
+        @tweet_got = Tweet.find(params[:id])
+        render json: @tweet_got
+      end
+
+      def create
+        @tweet_new = Tweet.new(tweet_params)
+        @tweet_new.user_id = current_user.id
+        @tweet_new.save
+
+        render json: @tweet_new, status: :created
+      end
+
+      def edit
+        @tweet_got = Tweet.find(params[:id])
+        authorize @tweet_got
+        @tweet_parent = @tweet_got.replied_to
+      end
+
+      def update
+        @tweet_new = Tweet.find(params[:id])
+        authorize @tweet_new
+        @tweet_new.update(tweet_params)
+
+        render json: @tweet_new
+      end
+
+      def destroy
+        @tweet_new = Tweet.find(params[:id])
+        authorize @tweet_new
+        @tweet_new.destroy
+      end
+
+      private
+
+      def tweet_params
+        params.require(:tweet).permit(:body, :replied_to_id)
+      end
+    end
+  end
+end
