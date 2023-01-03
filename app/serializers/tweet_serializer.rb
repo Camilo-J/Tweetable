0172b78af315b09695a0d4c47aa451e7c6591c95
Camilo-J <josecamilo1902@outlook.com>
2023-01-03 15:59:51 -0500

    Add serializer for user in tweet and user serialiers.rb

--- app/serializers/tweet_serializer.rb
+++ app/serializers/tweet_serializer.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class TweetSerializer < ActiveModel::Serializer
-  attributes :id, :body, :replies_count, :likes_count, :user_id, :created_at, :updated_at,
-             :user_image, :likes
-  def user_image
-    object.user.avatar.service_url
+  attributes :id, :body, :replies_count, :likes_count, :user_id, :updated_at,
+             :user_data, :likes
+  def user_data
+    { user_image: object.user.avatar.service_url, name: object.user.name,
+      username: object.user.username }
   end
 end
+# ,name: object.user.name,
+#     username: object.user.username
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :username, :name, :role, :avatar
+  attributes :id, :email, :username, :name, :role, :user_image, :tweets
+  def user_image
+    object.avatar.service_url
+  end
 end
